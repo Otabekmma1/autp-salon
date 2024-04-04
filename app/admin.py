bc0ed70ebef3529a2b2dcd9beec83e120a476720
Otabek <otabek.qosimov0101@gmail.com>
2024-04-04 21:23:09 +0300

    second commit

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Colour, Brand, Car
 
 class BrandAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'image')
+    list_display = ('id', 'name', 'get_image')
     list_filter = ('name',)
     search_fields = ('id', 'name')
+
+    def get_image(self, obj):
+        return mark_safe(f"<img src='{obj.image.url}' width='75'> ")
 
 class ColourAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
@@ -14,9 +19,12 @@ class ColourAdmin(admin.ModelAdmin):
 
 
 class CarAdmin(admin.ModelAdmin):
-    list_display = ('id', 'model', 'colour', 'brand', 'price', 'price_types', 'image')
+    list_display = ('id', 'model', 'colour', 'brand', 'price', 'price_types', 'get_image')
     list_filter = ('brand', 'colour')
     search_fields = ('id', 'model', 'brand')
+
+    def get_image(self, obj):
+        return mark_safe(f"<img src='{obj.image.url}' width='75'> ")
 
 admin.site.register(Brand, BrandAdmin)
 admin.site.register(Colour, ColourAdmin)
